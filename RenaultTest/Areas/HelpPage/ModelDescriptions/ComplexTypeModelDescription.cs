db58a2d08f22c5f41b80a1f7a5a46d3b1090b8c3
Germán Gonzalez Martínez <ggonzalez@nimtech.com.mx>
2019-03-05 18:21:40 -0600

    Agregar archivos de proyecto.

---
+++ RenaultTest/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace RenaultTest.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
