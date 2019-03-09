5c728fdde2aa8a3d276973a32ec7656b125276ec
Germán Gonzalez Martínez <ghizmox@hotmail.com>
2019-03-09 12:58:16 -0600

    Commited By
    Signed-off-by: Germán Gonzalez Martínez <ghizmox@hotmail.com>

--- RenaultTest/Migrations/Configuration.cs
+++ RenaultTest/Migrations/Configuration.cs
@@ -1,23 +1,54 @@
 namespace RenaultTest.Migrations
 {
-    using System;
-    using System.Data.Entity;
+    using global::Models;
+    using Models;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<RenaultTest.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(RenaultTest.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            context.Subject.AddOrUpdate(x => x.ID,
+                new Subject() { ID = 1, Name = "Matemáticas" },
+                new Subject() { ID = 1, Name = "Historia" },
+                new Subject() { ID = 1, Name = "Español" },
+                new Subject() { ID = 1, Name = "Derecho" }
+                );
+            context.Student.AddOrUpdate(x => x.ID,
+                new Student() { ID = 1, Name = "John", LastName = "Doe" },
+                new Student() { ID = 1, Name = "Mary", LastName = "Ditt" }
+                );
+            context.Grade.AddOrUpdate(x => x.ID,
+                new Grade() { ID = 1, Subject_ID = 1, Student_ID = 1, Score = 7, Partial = 1 },
+                new Grade() { ID = 2, Subject_ID = 1, Student_ID = 1, Score = 10, Partial = 2 },
+                new Grade() { ID = 3, Subject_ID = 1, Student_ID = 1, Score = 8, Partial = 3 },
+                new Grade() { ID = 4, Subject_ID = 2, Student_ID = 1, Score = 6, Partial = 1 },
+                new Grade() { ID = 5, Subject_ID = 2, Student_ID = 1, Score = 8, Partial = 2 },
+                new Grade() { ID = 6, Subject_ID = 2, Student_ID = 1, Score = 9, Partial = 3 },
+                new Grade() { ID = 7, Subject_ID = 3, Student_ID = 1, Score = 7, Partial = 1 },
+                new Grade() { ID = 8, Subject_ID = 3, Student_ID = 1, Score = 10, Partial = 2 },
+                new Grade() { ID = 9, Subject_ID = 3, Student_ID = 1, Score = 8, Partial = 3 },
+                new Grade() { ID = 10, Subject_ID = 4, Student_ID = 1, Score = 9, Partial = 1 },
+                new Grade() { ID = 11, Subject_ID = 4, Student_ID = 1, Score = 8, Partial = 2 },
+                new Grade() { ID = 12, Subject_ID = 4, Student_ID = 1, Score = 8, Partial = 3 },
+                new Grade() { ID = 1, Subject_ID = 1, Student_ID = 2, Score = 7, Partial = 1 },
+                new Grade() { ID = 2, Subject_ID = 1, Student_ID = 2, Score = 10, Partial = 2 },
+                new Grade() { ID = 3, Subject_ID = 1, Student_ID = 2, Score = 8, Partial = 3 },
+                new Grade() { ID = 4, Subject_ID = 2, Student_ID = 2, Score = 6, Partial = 1 },
+                new Grade() { ID = 5, Subject_ID = 2, Student_ID = 2, Score = 8, Partial = 2 },
+                new Grade() { ID = 6, Subject_ID = 2, Student_ID = 2, Score = 9, Partial = 3 },
+                new Grade() { ID = 7, Subject_ID = 3, Student_ID = 2, Score = 7, Partial = 1 },
+                new Grade() { ID = 8, Subject_ID = 3, Student_ID = 2, Score = 10, Partial = 2 },
+                new Grade() { ID = 9, Subject_ID = 3, Student_ID = 2, Score = 8, Partial = 3 },
+                new Grade() { ID = 10, Subject_ID = 4, Student_ID = 2, Score = 9, Partial = 1 },
+                new Grade() { ID = 11, Subject_ID = 4, Student_ID = 2, Score = 8, Partial = 2 },
+                new Grade() { ID = 12, Subject_ID = 4, Student_ID = 2, Score = 8, Partial = 3 }
+                );
         }
     }
 }
